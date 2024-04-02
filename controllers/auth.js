0b80b6cf64ff0d6ba466b095ececf2f6523df676
enn-dee <nddar191131@gmail.com>
2024-04-02 16:23:27 +0530

    Login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
 import User from "../models/User.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import { config } from "dotenv";
+config();
+const KEY = process.env.JSON_KEY;
 
 export const signup = async (req, res) => {
   try {
@@ -19,8 +24,12 @@ export const signup = async (req, res) => {
     const newUser = new User({ email, username, password: hashedPass });
     await newUser.save();
 
+    const tokenPayload = { id: newUser._id, email: newUser.email }; 
+    const token = jwt.sign(tokenPayload, KEY, { expiresIn: "1h" });
+
     return res.status(201).json({
       success: `User created successfully `,
+      token: token,
     });
   } catch (error) {
     console.error("Error in signup controller: ", error.message);
@@ -29,5 +38,21 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      await bcrypt.compare(user.password);
+
+      return res.status(200).json({ error: "Logged In" });
+    } else {
+      return res.status(400).json({ error: "User doesn't exist" });
+    }
+  } catch (error) {
+    console.log("Error in login controller: ", error);
+    res.status(501).json({ error: "Internal server error" });
+  }
 };
--- routes/AttendanceRoute.js
+++ routes/AttendanceRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.js";
+import { login, signup } from "../controllers/auth.js";
 
 const route = express.Router();
 
-
 route.post("/signup", signup);
+route.post("/login", login);
 
-export default route
+export default route;
